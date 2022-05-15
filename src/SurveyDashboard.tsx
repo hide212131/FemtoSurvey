76265b703227a26ff560b19160cc876578aef317
Hideyuki Kagami <hide212131@gmail.com>
2022-05-15 22:32:48 +0900

    add delete button.

--- src/SurveyDashboard.tsx
+++ src/SurveyDashboard.tsx
@@ -19,7 +19,18 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Button,
+  Backdrop,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
 } from "@material-ui/core";
+import { deleteSurveyForm } from "./graphql/mutations";
+import Delete from "@material-ui/icons/Delete";
 //Define Survey JSON
 //Here is the simplest Survey with one text question
 const useStyles = makeStyles((theme) => ({
@@ -96,6 +107,9 @@ const SurveyDashboard = () => {
   const classes = useStyles();
   const [createdSurveyForms, setCreatedSurveyForms] = useState<Form[]>();
   const [createdSurveyInputs, setCreatedSurveyInputs] = useState<Input[]>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [message, setMessage] = useState<string | null>();
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     Auth.currentAuthenticatedUser({
@@ -146,6 +160,47 @@ const SurveyDashboard = () => {
     setCreatedSurveyInputs(
       (result.data?.listSurveyInputs?.items as unknown) as Input[]
     );
+  };
+
+  // dialog
+  const [open, setOpen] = React.useState(false);
+  const [deleteText, setDeleteText] = React.useState("");
+  const [deleteForm, setDeleteForm] = React.useState<Form>();
+
+  const handleClickOpen = (form: Form) => {
+    setDeleteForm(form)
+    setDeleteText("");
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setDeleteForm(undefined)
+    setDeleteText("");
+    setOpen(false);
+  };
+
+  // delete all
+  const handleDeleteAll = async () => {
+    const formID = deleteForm?.id;
+    try {
+      setLoading(true);
+      if (formID) {
+        const input = {
+          id: formID,
+        };
+
+        // await API.graphql(graphqlOperation(deleteSurveyForm, { input }));
+
+        
+        window.location.reload();
+      }
+      setMessage("success");
+    } catch (err: any) {
+      console.log(err);
+      setMessage("error deleteting survey:" + err.errors);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -202,6 +257,7 @@ const SurveyDashboard = () => {
                 <TableCell>Form</TableCell>
                 <TableCell>Results</TableCell>
                 <TableCell>Anser by yourself</TableCell>
+                <TableCell></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -218,11 +274,57 @@ const SurveyDashboard = () => {
                   <TableCell>
                     <Link to={"input/" + item.inputKey}>click me</Link>
                   </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      startIcon={<Delete />}
+                      onClick={() => handleClickOpen(item)}
+                      disabled={loading}
+                    >
+                      DELETE ALL
+                    </Button>
+                    <Backdrop className={classes.backdrop} open={loading}>
+                      <CircularProgress color="inherit" />
+                    </Backdrop>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </Paper>
+      </div>
+      <div>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Delete { deleteForm?.name }'s Form and Inputs</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To confirm deletion, please enter "Delete" in the field.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              fullWidth
+              variant="standard"
+              label="Delete"
+              onChange={(event) => setDeleteText(event.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<Delete />}
+              disabled={loading || deleteText !== "Delete"}
+              onClick={handleDeleteAll}
+            >
+              DELETE ALL
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </React.Fragment>
   );
